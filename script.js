536b47d59abec40bbf2e15dda0aa8661e9a7e01d
pdavisDU <pdavisdu@gmail.com>
2022-08-21 13:38:06 -0500

    first commit, set up boiler and logic

---
+++ script.js
@@ -0,0 +1,10 @@
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+var userChoice = prompt("please choose R,P,S");
+var options = ["R", "P", "S"];
+var computerChoice = options[Math.floor(Math.random() * types.length)];
+var computerChoice = alert;
+
+console.log(userChoice);
