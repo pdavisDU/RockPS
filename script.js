eb8e3810335b8a7b14e45346176189d7dd25bfc9
pdavisDU <pdavisdu@gmail.com>
2022-08-27 11:47:49 -0600

    added logic for the computer choice

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ var wins = "";
 var losses = "";
 var ties = "";
 
-var userChoice = prompt("please choose R,P,S");
 var options = ["R", "P", "S"];
-var computerChoice = options[Math.floor(Math.random() * types.length)];
-var computerChoice = alert;
 
+
+var startGame = function() {
+    var userChoice = prompt("please choose R,P,S");
+    if (!userChoice) {
+        return;
+    }
+    
+    userChoice = userChoice.toUpperCase();
+    
+    var randomIndex = Math.floor(Math.random() * options.length);
+    var computerChoice = options[randomIndex];
+
+    window.alert('Computer chose ' + computerChoice) {
+        
+    }
+
+}
 console.log(userChoice);
