941bc65c6a88261b8c36e071f0b688cf5c6b0748
pdavisDU <pdavisdu@gmail.com>
2022-08-27 11:55:56 -0600

    added scoreboard with stats

--- script.js
+++ script.js
@@ -4,29 +4,35 @@ var ties = "";
 
 var options = ["R", "P", "S"];
 
+var startGame = function () {
+  var userChoice = prompt("please choose R,P,S");
+  if (!userChoice) {
+    return;
+  }
 
-var startGame = function() {
-    var userChoice = prompt("please choose R,P,S");
-    if (!userChoice) {
-        return;
-    }
-    
-    userChoice = userChoice.toUpperCase();
-    
-    var randomIndex = Math.floor(Math.random() * options.length);
-    var computerChoice = options[randomIndex];
+  userChoice = userChoice.toUpperCase();
 
-    window.alert('Computer chose ' + computerChoice)
+  var randomIndex = Math.floor(Math.random() * options.length);
+  var computerChoice = options[randomIndex];
 
-    if (userChoice === computerChoice) {
-        ties++;
-        window.alert('Its a tie');
+  window.alert("Computer chose " + computerChoice);
 
-    } else if (
-        (userChoice === 'R' && computerChoice === 'S') ||
-        (userChoice === 'P' && computerChoice === 'R') ||
-        (userChoice === 'S' && computerChoice === 'P')
-        ) {
-            wins++;
-            window.alert('You Win!');
-}
+  if (userChoice === computerChoice) {
+    ties++;
+    window.alert("Its a tie");
+  } else if (
+    (userChoice === "R" && computerChoice === "S") ||
+    (userChoice === "P" && computerChoice === "R") ||
+    (userChoice === "S" && computerChoice === "P")
+  ) {
+    wins++;
+    window.alert("You Win!");
+  } else {
+    losses++;
+    window.alert("You Lose!");
+  }
+
+  window.alert(
+    "Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties
+  );
+};
